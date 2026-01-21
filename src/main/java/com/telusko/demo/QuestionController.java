8ba3169e56fdfcb5d9ca83eea118fba8586457ca
Ravindu Deshan Perera <deshr.perera03@gmail.com>
2026-01-21 23:38:55 +0530

    successfully repond coming to web using port (only for testing purpose)

--- src/main/java/com/telusko/demo/QuestionController.java
+++ src/main/java/com/telusko/demo/QuestionController.java
@@ -1,4 +1,19 @@
 package com.telusko.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
 public class QuestionController {
+
+    @GetMapping("allQuestions")
+
+    public String getAllQuestion(){
+
+        return "hi this logic works perfectly";
+
+    }
+
 }
